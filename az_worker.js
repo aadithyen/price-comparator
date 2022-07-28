78c498738c7d81f8a129d532cdbf6cab528576db
Aadithye N <aadithyen@gmail.com>
2022-07-29 00:43:29 +0530

    Collects price data and sends to server

---
+++ az_worker.js
@@ -0,0 +1,35 @@
+const init = () => {
+	time = (Date.now() / 1000).toString();
+	title = document
+		.querySelector("#productTitle")
+		.innerHTML.substring(0, 50)
+		.trim();
+	site = "amazon";
+	price = document
+		.querySelector(".apexPriceToPay .a-offscreen")
+		.innerHTML.substring(1)
+		.replace(/,/g, "");
+
+	url = document.location.origin + document.location.pathname;
+
+	fetch("http://localhost:3000/addEntry", {
+		method: "POST",
+		headers: {
+			Accept: "application/json",
+			"Content-Type": "application/json",
+		},
+		body: JSON.stringify({
+			title: title,
+			site: site,
+			timestamp: time,
+			url: url,
+			price: price,
+		}),
+	})
+		.then((data) => data.json())
+		.then((res) => {
+			console.log(res);
+		});
+};
+
+init();
---
+++ fk_worker.js
@@ -0,0 +1,35 @@
+const init = () => {
+	time = (Date.now() / 1000).toString();
+	title = document
+		.querySelector(".B_NuCI")
+		.innerHTML.replace(/&nbsp;/g, "")
+		.substring(0, 50);
+	site = "flipkart";
+	price = document
+		.querySelector("._30jeq3._16Jk6d")
+		.innerHTML.substring(1)
+		.replace(/,/g, "");
+
+	url = document.location.origin + document.location.pathname;
+
+	fetch("http://localhost:3000/addEntry", {
+		method: "POST",
+		headers: {
+			Accept: "application/json",
+			"Content-Type": "application/json",
+		},
+		body: JSON.stringify({
+			title: title,
+			site: site,
+			timestamp: time,
+			url: url,
+			price: price,
+		}),
+	})
+		.then((data) => data.json())
+		.then((res) => {
+			console.log(res);
+		});
+};
+
+init();
